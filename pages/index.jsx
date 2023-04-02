c2b3c65677993054bf05a85d1bd9e81b44e55032
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-02 12:27:04 -0600

    Aplicando layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import { MainLayout } from '@/components/layouts/MainLayout'
 
@@ -9,10 +8,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function HomePage() {
   return (
     <MainLayout>
-      <div className={styles.grid}>
+      <div className='grid'>
         <h2 className={inter.className}>Home Page</h2>
         
-        <Link href={"/about"} className= {styles.card} replace>
+        <Link href={"/about"} className= 'card' replace>
           
           <h2 className={inter.className}>Ir a About</h2>
         </Link>
