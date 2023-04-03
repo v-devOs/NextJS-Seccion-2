04732a1db7341efb1ca2c047d3b6b2be510820e8
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-04-03 15:07:00 -0600

    Fin de la mudacion

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import '../styles/globals.css'
+import { ReactElement } from 'react'
+import { AppProps } from 'next/app'
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: ( page: ReactElement ) => JSX.Element
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout || (page => page)
+
+  return getLayout(<Component {...pageProps} />)
+}
